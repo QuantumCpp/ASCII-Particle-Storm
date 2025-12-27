f73ac038a10c35bbe8fa625e42fdd48aae69927f
QuantumCpp <jeremynobel16@gmail.com>
2025-12-26 21:25:47 -0500

    Refactor rain mode: remove cpp, update header, adjust main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <raylib.h>
 #include <raymath.h>
+#include "rain-mode/rain-ascii.h"
 
 int main(){
   const int ScreenWidth = 500;
   const int ScreenHeight = 500;
+  RainAscii Rain; 
 
   InitWindow(ScreenWidth, ScreenHeight, "ASCII-Particle-Storm");
   SetTargetFPS(60);
@@ -11,6 +13,7 @@ int main(){
   while(!WindowShouldClose()){
     BeginDrawing();
       ClearBackground(BLACK);
+      Rain.CreateObject();
     EndDrawing();
   }
 
--- rain-mode/rain-ascii.h
+++ rain-mode/rain-ascii.h
@@ -1,9 +1,25 @@
 #include "../General-utilities/GeneralUtilities.h"
+#include <raylib.h>
+
 
 
 class RainAscii{
   private:
+    Vector2 dimension = {5.0, 5.0};
+    Vector2 Posicion = {(float(WidthScreen)/2), (float(HeigthScreen)/2)};
+    
+    
 
   public:
+    void CreateObject(){
+      //Linea vertical derecha
+      DrawLine(int(Posicion.x - dimension.x), int(Posicion.y - dimension.y), int(Posicion.x - dimension.x), int(Posicion.y + dimension.y), WHITE);
+      //Linea vertical izquierda
+      DrawLine(int(Posicion.x + dimension.x), int(Posicion.y - dimension.y), int(Posicion.x + dimension.x), int(Posicion.y + dimension.y), WHITE);
+      //Linea horizontal inferior
+      DrawLine(int(Posicion.x - dimension.x), int(Posicion.y + dimension.y), int(Posicion.x + dimension.x), int(Posicion.y + dimension.y), WHITE);
+      //Linea horizontal superior
+      DrawLine(int(Posicion.x - dimension.x), int(Posicion.y - dimension.y), int(Posicion.x + dimension.x), int(Posicion.y - dimension.y), WHITE);
+    }
 
 };
